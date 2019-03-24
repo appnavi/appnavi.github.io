7b6a210abc3fab1e2f9420f62c7f28fca3eeb70f
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2019-03-24 22:05:02 +0900

    my commit

---
+++ build/precache-manifest.675cfa619c1507cae856ada636039947.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "88393153e15770eb0cce",
+    "url": "/appnavihomepage/static/css/main.367ab3ad.chunk.css"
+  },
+  {
+    "revision": "88393153e15770eb0cce",
+    "url": "/appnavihomepage/static/js/main.3a496d64.chunk.js"
+  },
+  {
+    "revision": "4370ed0b3c588d6bf4d4",
+    "url": "/appnavihomepage/static/js/runtime~main.611b7c58.js"
+  },
+  {
+    "revision": "5b601115e2bd3da8d357",
+    "url": "/appnavihomepage/static/js/2.e9e31bc6.chunk.js"
+  },
+  {
+    "revision": "3a638e8b8dddd421df7930e82039585c",
+    "url": "/appnavihomepage/static/media/newnavi.3a638e8b.png"
+  },
+  {
+    "revision": "3669ffb25fc50f1bd5aa4dbd6dd0e0db",
+    "url": "/appnavihomepage/static/media/logo.3669ffb2.png"
+  },
+  {
+    "revision": "f0c0393b861bc9884318a5f2e23dba07",
+    "url": "/appnavihomepage/static/media/Home.f0c0393b.png"
+  },
+  {
+    "revision": "85ee555a26089581b420edff0ba0d2f0",
+    "url": "/appnavihomepage/static/media/osaka.85ee555a.png"
+  },
+  {
+    "revision": "dc21b9be7d021c6667e7c5acec09c8e3",
+    "url": "/appnavihomepage/static/media/fooder.dc21b9be.png"
+  },
+  {
+    "revision": "54d2b63a6a43911a9dcd59ba29616c6d",
+    "url": "/appnavihomepage/static/media/name.54d2b63a.png"
+  },
+  {
+    "revision": "a129a98dad1f0796060e686e45540c0e",
+    "url": "/appnavihomepage/static/media/s_Twitter2.a129a98d.JPG"
+  },
+  {
+    "revision": "c616ab05f8f65ba9b50c88430a273444",
+    "url": "/appnavihomepage/static/media/active1.c616ab05.png"
+  },
+  {
+    "revision": "64d9c55bcafac12ca32f7eb5224ccf84",
+    "url": "/appnavihomepage/static/media/newshomeimg.64d9c55b.png"
+  },
+  {
+    "revision": "d30111e5f06dc9e9ca051cffd69db62b",
+    "url": "/appnavihomepage/static/media/hackathon.d30111e5.png"
+  },
+  {
+    "revision": "1d6b512a809a823dcbfa05f32a3167dc",
+    "url": "/appnavihomepage/static/media/contact.1d6b512a.png"
+  },
+  {
+    "revision": "fec1f2863c51be8a31efbfc2656f4333",
+    "url": "/appnavihomepage/index.html"
+  }
+];
